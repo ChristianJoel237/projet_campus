ccaf41db19f22a552b4533edc9eabe91fa770635
ChristianJoel237 <nankepbawou@gmail.com>
2026-06-30 16:15:06 +0100

    mise a jour

--- admin_interface/src/services/authService.ts
+++ admin_interface/src/services/authService.ts
@@ -1,24 +1,35 @@
-// import { api } from './apiConfig';
+ import { api } from './apiConfig';
 
 export interface ReponseConnexion {
   token: string;
 }
 
 export const authService = {
-  seConnecter: (
+  seConnecter: async(
     email: string,
     motDePasse: string
   ): Promise<ReponseConnexion> => {
     // Backend not connected yet. Use mock data locally for development.
     // When ready, uncomment the API call below and remove the mock implementation.
 
-    // return api.post<ReponseConnexion>('/auth/login', { email, motDePasse }, false);
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({ token: 'mock-jwt-token-1234567890' });
-      }, 500);
-    });
+    const donnees = await api.post<ReponseConnexion>('/auth/login', { email, motDePasse }, false);
+    if (donnees && donnees.token){
+      localStorage.setItem('token',donnees.token)
+      localStorage.setItem('estConnecte','true')
+    }
+    return donnees 
+    //return new Promise((resolve) => {
+      //setTimeout(() => {
+       // resolve({ token: 'mock-jwt-token-1234567890' });
+     // }, 500);
+    //});
   },
+
+  deconnexion :() =>{
+    localStorage.removeItem('token');
+    localStorage.removeItem('estConnecte');
+    window.location.href='/connexion'
+  }
 };
 
 export default authService;
